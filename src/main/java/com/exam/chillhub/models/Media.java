cf94952d017c2dc6c4636093be1f12eef2617080
A-Guldborg <aguh@itu.dk>
2021-11-29 22:06:17 +0100

    Tilføjer year og rating til media

--- src/main/java/com/exam/chillhub/models/Media.java
+++ src/main/java/com/exam/chillhub/models/Media.java
@@ -8,20 +8,29 @@ import java.io.IOException;
 public abstract class Media {
     private final String name;
     private final MediaType type;
+    private final double rating;
+    private final String year;
 
-    public Media(String name, MediaType type) {
+    public Media(String name, MediaType type, double rating, String year) {
         this.name = name;
         this.type = type;
+        this.rating = rating;
+        this.year = year;
     }
     public String getName() {
         return this.name;
     }
-
     public MediaType getType() {
         return this.type;
     }
     public Image getPoster() throws IOException {
         var filename = "database/" + getType().name().toLowerCase() + "/" + getName() + ".jpg";
         return new Image(ChillhubApplication.class.getResource(filename).openStream());
+    }
+    public double getRating() {
+        return this.rating;
+    }
+    public String getYear() {
+        return this.year;
     }
 }
