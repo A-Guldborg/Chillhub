d1d844a6b48117fc741ac63dc2b2c412d314a132
A-Guldborg <aguh@itu.dk>
2021-11-29 19:31:20 +0100

    Tests for Filter class

--- src/main/java/com/exam/chillhub/models/Filter.java
+++ src/main/java/com/exam/chillhub/models/Filter.java
@@ -4,13 +4,25 @@ import java.util.*;
 public class Filter {
     private final String title;
     private List<Media> filteredData;
+    private HashMap<MediaType, List<Media>> cachedLists;
+
     public Filter(String title) {
         this.title = title;
         filteredData = new ArrayList<>();
+        cachedLists = new HashMap<>();
     }
 
+    /**
+     * Tilføjer et medie til filteret og tilføjer også mediet til en evt. cached filtrering af dette medies MediaType.
+     * @param media Det medie, der skal tilføjes til filteret.
+     */
     public void addToFilter(Media media) {
         filteredData.add(media);
+
+        // Tjekker om en cached liste er lavet over denne MediaType og tilføjer i så fald mediet
+        if (cachedLists.containsKey(media.getType())) {
+            cachedLists.get(media.getType()).add(media);
+        }
     }
 
     public String getTitle() {
@@ -19,5 +31,29 @@ public class Filter {
 
     public List<Media> getFilteredData() {
         return this.filteredData;
+    }
+
+    /**
+     * Returnerer en liste over det nuværende filter hvor kun serier bliver vist.
+     * Metoden gemmer listen så den hurtigere kan blive fundet frem næste gang den samme søgning laves i samme session.
+     *
+     * @param type MediaType enum som filtreringsgrundlaget
+     * @return List<Media> Listen over den filtrerede søgning
+     */
+    public List<Media> getFilteredType(MediaType type) {
+        // Tjekker om listen allerede er skabt
+        if (cachedLists.containsKey(type)) {
+            return cachedLists.get(type);
+        } else {
+            // Alternativt søger den igennem filteret og tilføjer de enkelte elementer til en List der caches i et HashMap
+            List<Media> filteredByType = new ArrayList<>();
+            for (Media media : filteredData) {
+                if (media.getType() == type) {
+                    filteredByType.add(media);
+                }
+            }
+            cachedLists.put(type, filteredByType);
+            return filteredByType;
+        }
     }
 }
--- src/main/tests/src/models/FilterTests.java
+++ src/main/tests/src/models/FilterTests.java
@@ -1,17 +1,53 @@
 package models;
 
-import com.exam.chillhub.models.Filter;
-import com.exam.chillhub.models.Movie;
+import com.exam.chillhub.models.*;
 
+import com.exam.chillhub.models.Series;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FilterTests {
+    private Filter filter;
+
+    @BeforeEach
+    public void setUp() {
+        // Start filter med 5 forskellige medier, både film og serier
+        filter = new Filter("TestFilter");
+        for (int i = 1; i <= 5; i++) {
+            Media m;
+            if (i % 2 == 1) {
+                m = new Movie("Test" + i);
+            } else {
+                m = new Series("Test" + i);
+            }
+            filter.addToFilter(m);
+        }
+    }
+
     @Test
     public void testAddToFilter() {
         Movie mov = new Movie("Test123");
-        Filter filter = new Filter("TestFilter");
         filter.addToFilter(mov);
         assertTrue(filter.getFilteredData().contains(mov));
+    }
+
+    @Test
+    public void testFilteredByType_Movie() {
+        List<Media> filteredList = filter.getFilteredType(MediaType.MOVIE);
+        for (Media m : filteredList) {
+            assertEquals(m.getType(), MediaType.MOVIE);
+        }
+    }
+
+    @Test
+    public void testFilteredByType_Series() {
+        List<Media> filteredList = filter.getFilteredType(MediaType.SERIES);
+        for (Media m : filteredList) {
+            assertEquals(m.getType(), MediaType.SERIES);
+        }
     }
 }
